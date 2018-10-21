0b4f6e755893f4cca4e63aff6e984b3ebe186809
Kun Zhang <kunzhang1110@gmail.com>
2018-10-22 08:29:18 +1100

    v1.1 add splash screen

--- Project/Easy_Diet/app/src/main/java/au/edu/sydney/comp5216/easydiet/login/MainActivity.java
+++ Project/Easy_Diet/app/src/main/java/au/edu/sydney/comp5216/easydiet/login/MainActivity.java
@@ -1,27 +1,36 @@
 package au.edu.sydney.comp5216.easydiet.login;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import au.edu.sydney.comp5216.easydiet.R;
 
-/**
- * Main Greeting Page
- */
 
 public class MainActivity extends AppCompatActivity {
+
+    final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: Stay login mechanism
-        if(true){
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            MainActivity.this.startActivity(intent);
-        }
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+//        if(true){
+//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//            MainActivity.this.startActivity(intent);
+//        }
 
 
     }
